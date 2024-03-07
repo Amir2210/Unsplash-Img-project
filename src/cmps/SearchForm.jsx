c243036e641d8d9c9639be88d0a9b1d8c37baa62
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-03-07 13:26:14 +0200

    Create a form with an input and a submit button

--- src/cmps/SearchForm.jsx
+++ src/cmps/SearchForm.jsx
@@ -1,5 +1,22 @@
+
 export function SearchForm() {
+  function handleSubmit(ev) {
+    ev.preventDefault()
+    const searchValue = ev.target.elements.search.value
+    if (!searchValue) return
+    console.log(searchValue)
+  }
   return (
-    <h1>SearchForm</h1>
+    <section>
+      <h1 className='title'>Unsplash Images</h1>
+      <form className='search-form' onSubmit={handleSubmit}>
+        <input className='form-input search-input'
+          type="text"
+          placeholder='cat'
+          name='search'
+        />
+        <button type='submit' className='btn'>search</button>
+      </form>
+    </section>
   )
 }
