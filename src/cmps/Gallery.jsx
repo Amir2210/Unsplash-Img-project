bebc08b3382b3814a6af00385c9cf387a7c02cdb
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-03-07 13:43:06 +0200

    when error or no results return message to the user

--- src/cmps/Gallery.jsx
+++ src/cmps/Gallery.jsx
@@ -2,10 +2,14 @@ import { useFetchPhotos } from '../service/GetPhotos'
 
 export function Gallery() {
   const { isLoading, data, isError, error } = useFetchPhotos()
+  // console.log(data)
   if (!data) return <div>Loading...</div>
+  if (data.isError) return <h2>There was an error...</h2>
+  const results = data.data.results
+  if (!results.length) return <h4>No results founds...</h4>
   return (
     <section className='image-container'>
-      {data.data.results.map(img => <img className='img' key={img.id} src={img.links.download}></img>)}
+      {results.map(img => <img className='img' key={img.id} src={img.links.download} alt={img.alt_description}></img>)}
     </section>
   )
 }
