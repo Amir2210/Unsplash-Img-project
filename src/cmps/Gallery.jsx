e3ad151dbda416be0099d6c6494cdee5a314889b
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-03-10 18:30:15 +0200

    Add loader

--- src/cmps/Gallery.jsx
+++ src/cmps/Gallery.jsx
@@ -1,8 +1,20 @@
 import { useFetchPhotos } from '../service/GetPhotos'
-
+import { Hourglass } from 'react-loader-spinner'
 export function Gallery() {
   const { isLoading, data, isError, error } = useFetchPhotos()
-  if (!data) return <div>Loading...</div>
+  if (!data) return (
+    <div className='loader'>
+      <Hourglass
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="hourglass-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        colors={['#306cce', '#72a1ed']}
+      />
+    </div>
+  )
   if (data.isError) return <h2>There was an error...</h2>
   const results = data.data.results
   if (!results.length) return <h4>No results founds...</h4>
